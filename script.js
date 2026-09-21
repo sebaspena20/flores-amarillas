13c19c209fc161de89a7c75d9ebeb88deda8cef9
sebaspena20 <148934034+sebaspena20@users.noreply.github.com>
2026-09-21 13:15:07 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -379,7 +379,7 @@ function mostrarRespuestaComida(
         pizza:
             "Sabía que había algo importante que descubrir aquí. 🍕",
 
-        labandera:
+        bandera:
             "Una elección bastante seria. 😂🍗",
 
         sushi:
